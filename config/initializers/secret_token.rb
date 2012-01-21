bc82b820e584d2672c9cc053fa97ef3a40b99d85
Thomas Schrader <slogmen@gmail.com>
2012-01-22 00:57:20 +0100

    initial commit. new rails project created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yaws::Application.config.secret_token = 'b7febe54330ce320acdacb45a1ce5b1b07c744b9f43cad9f699a19bf7494b882ba15168e24f30a9e655c0aaf4c59f64ddbfa143b8fe402cf9b615a6cfe8ce7ea'

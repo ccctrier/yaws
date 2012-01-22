088573e1dc4966b26fb8263d6a274b5104e6e6d5
Thomas Schrader <slogmen@gmail.com>
2012-01-22 01:27:48 +0100

    removed ActiveRecord.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Because of CouchDB we don't need ActiveRecord
+#require 'rails/all'
+
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line

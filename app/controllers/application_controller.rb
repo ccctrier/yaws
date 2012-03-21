3e5934ae1ab6460e15f25e47f0ee8fd6eb8ef063
Juice <juice@c3t.de>
2012-03-21 23:50:47 +0100

    add cancan rescue redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,28 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
   
-    private
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to :login
+  end
   
-    def current_user_session
-      return @current_user_session if defined?(@current_user_session)
-      @current_user_session = UserSession.find
-    end
-  
-    def current_user
-      return @current_user if defined?(@current_user)
-      @current_user = current_user_session && current_user_session.record
-    end
-  
-    def render_404
-     respond_to do |format|
-       format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found }
-     format.xml  { head :not_found }
-       format.any  { head :not_found }
-     end
+  private
+
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
+  end
+
+  def render_404
+   respond_to do |format|
+     format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found }
+   format.xml  { head :not_found }
+     format.any  { head :not_found }
    end
-   
+ end
+
 end

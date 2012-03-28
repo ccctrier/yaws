61ac6282903280ae1a327c53731dc52f47b995b4
Thomas Schrader <slogmen@gmail.com>
2012-03-28 23:39:58 +0200

    added type attribute to posts model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120321234538) do
+ActiveRecord::Schema.define(:version => 20120328210600) do
 
   create_table "abilities", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20120321234538) do
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "type"
   end
 
   create_table "roles", :force => true do |t|

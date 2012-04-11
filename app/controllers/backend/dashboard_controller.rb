eaa32847857eddd9a87eb4288c5144b9944e3b69
Thomas Schrader <slogmen@gmail.com>
2012-04-11 23:59:08 +0200

    prepared dashboard controller.

--- app/controllers/backend/dashboard_controller.rb
+++ app/controllers/backend/dashboard_controller.rb
@@ -4,6 +4,14 @@ class Backend::DashboardController < ApplicationController
   
   def index
     authorize! :read, @backend
+    
+    @news = Post.where(:section => params[:type]).order("updated_at DESC").limit(10)
+    @blogposts = Post.where(:section => params[:type]).order("updated_at DESC").limit(10)
+    @events = Event.order("updated_at DESC").limit(10)
+    
+    respond_to do |format|
+      format.html
+    end    
   end
   
 end
